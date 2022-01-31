2dd929ce1b938e4dd5e471ffd0e3696302545519
Irina Kirillova <keira.kirillova@gmail.com>
2022-01-31 23:00:06 +0200

    add default markup for home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="container">
+                <header className="header">Header</header>
+                    <aside className="sidebar">
+                        <nav className="nav">
+                            <ul className="nav__list">
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">Profile</a>
+                                </li>
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">Messages</a>
+                                </li>
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">News</a>
+                                </li>
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">Music</a>
+                                </li>
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">Settings</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </aside>
+                <main className="main">Main</main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
