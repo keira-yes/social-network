d6bf528fcec282efb0abdfa8071cfa1963924bd5
Irina Kirillova <keira.kirillova@gmail.com>
2022-02-07 22:39:10 +0200

    move data to index.js

--- src/App.js
+++ src/App.js
@@ -5,27 +5,7 @@ import { Profile } from "./components/Profile/Profile";
 import { Messages } from './components/Messages/Messages';
 import './App.css';
 
-const posts = [
-    {id: 1, message: "Hello World!", likes: 5},
-    {id: 2, message: "My first post...", likes: 12}
-]
-
-const dialogs = [
-    {id: 1, name: "Name 1"},
-    {id: 2, name: "Name 2"},
-    {id: 3, name: "Name 3"},
-    {id: 4, name: "Name 4"},
-    {id: 5, name: "Name 5"},
-    {id: 6, name: "Name 6"}
-]
-
-const messages = [
-    {id: 1, message: "Hi world"},
-    {id: 2, message: "How are you doing"},
-    {id: 3, message: "Miss you"}
-]
-
-function App() {
+function App({ posts, dialogs, messages }) {
     return (
         <BrowserRouter>
             <div className="app">
--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const posts = [
+    {id: 1, message: "Hello World!", likes: 5},
+    {id: 2, message: "My first post...", likes: 12}
+]
+
+const dialogs = [
+    {id: 1, name: "Name 1"},
+    {id: 2, name: "Name 2"},
+    {id: 3, name: "Name 3"},
+    {id: 4, name: "Name 4"},
+    {id: 5, name: "Name 5"},
+    {id: 6, name: "Name 6"}
+]
+
+const messages = [
+    {id: 1, message: "Hi world"},
+    {id: 2, message: "How are you doing"},
+    {id: 3, message: "Miss you"}
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogs={dialogs} messages={messages} />
   </React.StrictMode>,
   document.getElementById('root')
 );
