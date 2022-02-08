293f9a7ed71d1825fbb032e7a8e549b53bc848d6
Irina Kirillova <keira.kirillova@gmail.com>
2022-02-08 19:10:52 +0200

    create ref to get textarea value in Posts

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,10 +1,23 @@
+import React from 'react';
 import{ Post } from "../Post/Post";
 import classes from './Posts.module.css';
 
 export const Posts = ({ posts }) => {
+    const newPostElem = React.createRef();
+
+    const addPost = () => {
+        const postContent = newPostElem.current.value;
+    }
+
     return (
-        <div className={classes.posts}>
-            {posts.map(item =>  <Post message={item.message} likes={item.likes} />)}
+        <div>
+            <div className="form">
+                <textarea ref={newPostElem} name="post" id="post" cols="30" rows="5"></textarea>
+                <button onClick={addPost}>Send</button>
+            </div>
+            <div className={classes.posts}>
+                {posts.map(item =>  <Post message={item.message} likes={item.likes} />)}
+            </div>
         </div>
     )
 }
