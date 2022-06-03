80592b41e406ec1fe65f754fa9575a8bf7450673
Irina Kirillova <keira.kirillova@gmail.com>
2022-06-03 21:42:11 +0200

    make Users as a class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,34 +1,38 @@
+import React from 'react';
 import axios from "axios";
 import avatar from "../../assets/images/avatar.png";
 
-export const Users = ({ users, getUsers, toggleFollowUserCreator, }) => {
+export class Users extends React.Component {
 
-    if (users.length === 0) {
+    componentDidMount() {
         axios("https://social-network.samuraijs.com/api/1.0/users").then(({ data }) => {
-            getUsers(data.items);
+            this.props.getUsers(data.items);
         });
     }
 
-    return (
-        <div>
-            {users.map(user => (
-                <article key={user.id}>
-                    <div>
-                        <figure>
-                            <img src={user.photos.small !==null ? user.photos.small : avatar} alt={user.name} />
-                        </figure>
-                        <button type="button" onClick={() => toggleFollowUserCreator(user.id)}>
-                            {user.followed ? "Unfollow" : "Follow"}
-                        </button>
-                    </div>
-                    <div>
-                        <h3>{user.name}</h3>
-                        <div>{"Country"}</div>
-                        <strong>{user.status}</strong>
-                        <div>{"City"}</div>
-                    </div>
-                </article>
-            ))}
-        </div>
-    )
+    render() {
+        const { users, toggleFollowUserCreator } = this.props;
+        return (
+            <div>
+                {users.map(user => (
+                    <article key={user.id}>
+                        <div>
+                            <figure>
+                                <img src={user.photos.small !==null ? user.photos.small : avatar} alt={user.name} />
+                            </figure>
+                            <button type="button" onClick={() => toggleFollowUserCreator(user.id)}>
+                                {user.followed ? "Unfollow" : "Follow"}
+                            </button>
+                        </div>
+                        <div>
+                            <h3>{user.name}</h3>
+                            <div>{"Country"}</div>
+                            <strong>{user.status}</strong>
+                            <div>{"City"}</div>
+                        </div>
+                    </article>
+                ))}
+            </div>
+        )
+    }
 }
