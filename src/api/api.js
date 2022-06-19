cf339f5764e3a415610549c834a08d06ca06bdf8
Irina Kirillova <keira.kirillova@gmail.com>
2022-06-19 12:55:23 +0200

    create instance of axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,35 +1,27 @@
 import axios from "axios";
 
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {"API-KEY": "70166802-e642-44ee-a14c-87b3ffa9a643"}
+})
+
 export const getAuthData = () => {
-    return axios(`https://social-network.samuraijs.com/api/1.0/auth/me`, { withCredentials: true })
-        .then(response => response.data);
+    return instance(`auth/me`).then(response => response.data);
 }
 
 export const getUsers = (usersCurrentPage = 1, usersPageLimit = 10) => {
-    return axios(`https://social-network.samuraijs.com/api/1.0/users?page=${usersCurrentPage}&count=${usersPageLimit}`,
-        { withCredentials: true })
-        .then(response => response.data);
+    return instance(`users?page=${usersCurrentPage}&count=${usersPageLimit}`).then(response => response.data);
 }
 
 export const getProfile = (id) => {
-    return axios(`https://social-network.samuraijs.com/api/1.0/profile/${id}`)
-        .then(response => response.data);
+    return instance(`profile/${id}`).then(response => response.data);
 }
 
 export const followUser = (id) => {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "70166802-e642-44ee-a14c-87b3ffa9a643"
-        }
-    }).then(response => response.data);
+    return instance.post(`follow/${id}`).then(response => response.data);
 }
 
 export const unFollowUser = (id) => {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "70166802-e642-44ee-a14c-87b3ffa9a643"
-        }
-    }).then(response => response.data);
+    return instance.delete(`follow/${id}`).then(response => response.data);
 }
