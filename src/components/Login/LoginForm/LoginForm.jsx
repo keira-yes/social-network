6ab5f9b0d50a68a42e168efbd326a75682734b6b
Irina Kirillova <keira.kirillova@gmail.com>
2022-06-25 21:26:38 +0200

    add validated fields to post form, login form

--- src/components/Login/LoginForm/LoginForm.jsx
+++ src/components/Login/LoginForm/LoginForm.jsx
@@ -1,19 +1,37 @@
 import { Field, reduxForm } from 'redux-form';
+import { Input } from "../../hoc/FormField/FormField";
+import {maxLength, required} from "../../../utils/validators";
+
+const maxLength16 = maxLength(16);
 
 const LoginForm = ({ handleSubmit }) => {
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="login">Login</label>
-                <Field name="login" component="input" type="text" />
+                <Field
+                    name="login"
+                    component={Input}
+                    type="text"
+                    validate={required}
+                />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <Field name="password" component="input" type="text" />
+                <Field
+                    name="password"
+                    component={Input}
+                    type="text"
+                    validate={[required, maxLength16]}
+                />
             </div>
             <div>
+                <Field
+                    name="rememberMe"
+                    component="input"
+                    type="checkbox"
+                />
                 <label htmlFor="rememberMe">Remember me</label>
-                <Field name="rememberMe" component="input" type="checkbox" />
             </div>
             <button type="submit">Login</button>
         </form>
