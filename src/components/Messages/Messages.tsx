b6e2c1c16c2e1cc9886ee88cbdb4f62e4b202fc3
Irina Kirillova <keira.kirillova@gmail.com>
2022-08-30 23:08:30 +0200

    sent message by WebSocket

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
 import Message from './Message/Message';
 
+const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx');
+
 const Messages = () => {
     const params = useParams();
+    const [message, setMessage] = useState('');
+
+    const handleSendMessage = () => {
+        if (!message.length) return;
+        ws.send(message);
+        setMessage('');
+    }
 
     return (
         <div>
             <h1>Chat with {params.id}</h1>
             <Message />
+            <div>
+                <textarea onChange={(e) => setMessage(e.target.value)} value={message}/>
+                <button onClick={handleSendMessage}>Send</button>
+            </div>
         </div>
     )
 }
